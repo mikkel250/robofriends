3e4aadea0b1d59dcbd9f19f39d5c51085e55c274
Mikkel Ridley <mikkel250@gmail.com>
2019-08-09 00:56:53 -0700

    Added styling and searchbox

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import {robots} from './robots.js'
-import CardList from './CardList';
+// import CardList from './CardList';
+import App from './App';
 import 'tachyons';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<CardList robots={robots} />, document.getElementById('root'));
+ReactDOM.render(<App robots={robots} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
