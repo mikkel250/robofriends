c8491f9d61120f0b3b5806c6692e23e0ec1418d8
Mikkel Ridley <mikkel250@gmail.com>
2019-09-10 14:15:01 -0700

    run build to deploy to gh pages

---
+++ build/precache-manifest.d0a6f1190267dce2e2f637648c99bbe2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82b10560b638009c968932767809726e",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "db2820c62b93ca917c43",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "dc187b81fc0b5f2f891d",
+    "url": "/robofriends/static/css/main.5b164a6b.chunk.css"
+  },
+  {
+    "revision": "db2820c62b93ca917c43",
+    "url": "/robofriends/static/js/2.0d566601.chunk.js"
+  },
+  {
+    "revision": "dc187b81fc0b5f2f891d",
+    "url": "/robofriends/static/js/main.158dd709.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
